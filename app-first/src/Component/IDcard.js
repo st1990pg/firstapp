0c545481fac2ab45843092db286763dbe9e8af98
Stefan <stefan.todorovic@bild-studio.net>
2020-02-10 21:25:35 +0100

    update

--- app-first/src/Component/IDcard.js
+++ app-first/src/Component/IDcard.js
@@ -1,12 +1,6 @@
 import React from "react";
 const IDcard = porps => {
-  return (
-    <div>
-      <div>{porps.user.name}</div>
-      <div>{porps.user.age}</div>
-      <img src={porps.user.img} alt="porps.user.name" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default IDcard;
